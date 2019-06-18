91331d79491adf2480331ae17f43229daedf9ac1
LJ <ljmarinscull@gmail.com>
2019-06-17 17:32:21 -0700

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <section class="hero has-text-centered is-primary">
+            <div class="hero-body">
+                <div class="container">
+                    <h1 class="title">PWA Perfumes LJ</h1>
+                    <h2 class="subtitle">Con todo el aroma de la Habana!</h2>
+                </div>
+            </div>
+        </section>
+        <nuxt/>
+    </div>
+</template>
+
+<style>
+    html {
+        font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    }
+</style>
+
